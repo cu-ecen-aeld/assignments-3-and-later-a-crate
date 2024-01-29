7cf2dee7f3becd530c0c1da4073131ab9ab0f681
Amelia Crate <ameliacrate@gmail.com>
2024-01-28 18:36:20 -0800

    assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
-
 set -e
 set -u
 
@@ -33,11 +32,9 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
-    # TODO: Add your kernel build steps here
+    ARCH=${ARCH} make defconfig
+    ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make ${MAKEOPTS:=}
 fi
-
-echo "Adding the Image in outdir"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +44,10 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs/{bin,boot,dev,etc,home,lib,lib64,sys,proc,root,sbin,sys,tmp,var/log,usr/bin,usr/sbin,usr/lib}
+
+echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +55,39 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make ${MAKEOPTS:=} distclean
+    CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make ${MAKEOPTS:=} defconfig
+    CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make ${MAKEOPTS:=}
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make ${MAKEOPTS:=} CONFIG_PREFIX=${OUTDIR}/rootfs/ ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a bin/busybox | grep 'program interpreter'
+cp --dereference $(${CROSS_COMPILE}gcc -print-sysroot)/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/ld-linux-aarch64.so.1
+cp --dereference $(${CROSS_COMPILE}gcc -print-sysroot)/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib64/ld-linux-aarch64.so.1
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | tr -d []
+for lib in $(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | tr -d []); do
+    path=$(find $(${CROSS_COMPILE}gcc -print-sysroot) -name $lib)
+    if [ "$path" == "" ]; then continue; fi
+    to=$(echo $path | sed "s#$(${CROSS_COMPILE}gcc -print-sysroot)#${OUTDIR}/rootfs#g")
+    echo "$path => $to"
+    cp --dereference $path $to
+done
 
-# TODO: Add library dependencies to rootfs
+cd $FINDER_APP_DIR
+make clean
+CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} make
+cp ./{writer,finder.sh,finder-test.sh,autorun-qemu.sh} ${OUTDIR}/rootfs/home/
+cp -r ../conf ${OUTDIR}/rootfs/home/
 
-# TODO: Make device nodes
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
-
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
-
-# TODO: Chown the root directory
-
-# TODO: Create initramfs.cpio.gz
+echo "Packing initrd"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
